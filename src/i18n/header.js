6e619e97242f376409ab3e407520457533519ca9
Eduardo Poot <eduardopoot83@gmail.com>
2025-07-21 19:40:19 -0600

    FInishing header

---
+++ src/i18n/header.js
@@ -0,0 +1,16 @@
+export const headerTranslations = {
+    es: {
+        about: 'Sobre mí',
+        skills: 'Habilidades',
+        projects: 'Proyectos',
+        contact: 'Contactar',
+        language: 'Idioma',
+    },
+    en: {
+        about: 'About Me',
+        skills: 'Skills',
+        projects: 'Projects',
+        contact: 'Contact',
+        language: 'Language',
+    },
+};

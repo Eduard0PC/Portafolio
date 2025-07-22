bcc6475fa0ec98e7dfbc814b8aeca911a4ac38f4
Eduardo Poot <eduardopoot83@gmail.com>
2025-07-21 20:27:09 -0600

    language change

---
+++ src/translations/index.js
@@ -0,0 +1,10 @@
+export const indexTranslations = {
+    es: {
+        description: 'Desarrollador Web',
+        cvButton: 'Descargar CV',
+    },
+    en:{
+        description: 'Web Developer',
+        cvButton: 'Download CV',
+    }
+}

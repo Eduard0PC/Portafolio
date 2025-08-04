c57246d2a9e5f0bf53440a91f00bc6a8cc84cb33
Eduardo Poot <eduardopoot83@gmail.com>
2025-08-04 17:57:21 -0600

    about finished v2

--- src/translations/index.js
+++ src/translations/index.js
@@ -4,19 +4,21 @@ export const indexTranslations = {
         cvButton: 'Descargar CV',
         about: {
             title: 'Sobre mí',
-            intro: 'Soy',
-            description: ', desarrollador web enfocado en construir experiencias digitales modernas, rápidas y accesibles.Tengo experiencia con tecnologías como',
-            closing: 'y siempre estoy en constante aprendizaje.'
-        }
+            intro: 'Hola soy',
+            description: ', desarrollador web enfocado en construir experiencias digitales modernas, rápidas y accesibles. Tengo experiencia con tecnologías',
+            closing: ', por lo que siempre estoy en constante aprendizaje.'
+        },
+        skills: 'Habilidades',
     },
     en: {
         description: 'Web Developer',
         cvButton: 'Download CV',
         about: {
             title: 'About Me',
-            intro: "I'm",
-            description: ', a web developer focused on building modern, fast, and accessible digital experiences. I have experience with technologies like',
-            closing: ", and I'm always learning and improving."
-        }
+            intro: "Hi i'm",
+            description: ', a web developer focused on building modern, fast, and accessible digital experiences. I have experience with',
+            closing: ", so i'm always learning and improving."
+        },
+        skills: 'Skills',
     }
 }
